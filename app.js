2b173f439a7d40ae1ee75d402630ca259529b009
ktd1230 <ktd1230@naver.com>
2020-01-05 21:55:46 +0900

    init

--- app.js
+++
@@ -1,7 +0,0 @@
-var express = require('express')
-var app = express()
-app.listen(8080, function() {
-    console.log("start! express server on port 8080");
-});
-
-console.log('end of server code....')
